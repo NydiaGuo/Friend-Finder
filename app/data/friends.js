a3662ecc28ff7391297c5edea11b209525a6a575
NI GUO <guoni0909@gmail.com>
2018-02-27 21:06:46 -0800

    update friends data file
    Adding more friends in the file

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,31 @@ var friends = [{
 },{
 	name:"Jhon",
 	photo:"https://media.giphy.com/media/pqodzWcYTrwXPexX8S/giphy.gif",
-	scores:['3', '2', '6', '4', '5', '1', '2', '5', '4', '1']
+	scores:['3', '2', '3', '4', '5', '1', '2', '5', '4', '1']
+},{
+	name:"Jenny",
+	photo:"https://media.giphy.com/media/3oFzmclQvU39JDbiTK/giphy.gif",
+	scores:['3', '2', '2', '4', '5', '1', '2', '5', '4', '1']
+},{
+	name:"Adam",
+	photo:"https://media.giphy.com/media/57Y0HrGWcu4WYvc6vE/giphy.gif",
+	scores:['3', '2', '5', '4', '5', '1', '2', '5', '4', '1']
+},{
+	name:"Lily",
+	photo:"https://media.giphy.com/media/l0HUljQc8aRP5iipa/giphy.gif",
+	scores:['3', '2', '3', '4', '4', '1', '2', '5', '4', '2']
+},{
+	name:"Laim",
+	photo:"https://media.giphy.com/media/3ohc0Rb3sjPi9Hw05W/giphy.gif",
+	scores:['3', '2', '4', '4', '5', '1', '2', '3', '4', '1']
+},{
+	name:"Huy",
+	photo:"https://media.giphy.com/media/8rElLG2h8FFqr2bvOm/giphy.gif",
+	scores:['2', '2', '3', '2', '1', '1', '2', '2', '3', '1']
+},{
+	name:"Amy",
+	photo:"https://media.giphy.com/media/5wFHW55Vdk2MCZSMIV/giphy.gif",
+	scores:['2', '2', '6', '3', '2', '1', '2', '5', '4', '1']
 }];
 
 module.exports = friends;
